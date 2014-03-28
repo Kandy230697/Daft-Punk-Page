fd58b7898e980f4c9d86e377d32b96cff22e25a6
Fili Martinez <fili06604@gmail.com>
2014-03-27 23:39:00 -0600

    Magia Ajax !
    Uso de YQL para poner en la pagina:
    Barrio, Ciudad y Pais.
    El error de los papas se cambio a un error que se le muestra al usuario
    y ya no a la consola.

--- JavaScript/Ajax.js
+++ JavaScript/Ajax.js
@@ -10,11 +10,34 @@ $(function(){
 	})
 });
 
-var base_url = "http://query/yahooapis.com/v1/public/yql?";
+var base_url = "http://query.yahooapis.com/v1/public/yql?";
 
 function obtenerGeoInfo(lat, longi){
 	var query = 'SELECT * FROM geo.placefinder WHERE text="'+lat+','+longi+'" AND gflags="R"';
-
 	query = encodeURIComponent(query);
-	console.log(query);
+
+	$.ajax({
+		url: base_url+"q="+query,
+		dataType : 'jsonp',
+		jsonpCallback: 'procesarGeoInfo',
+		data: {
+			format: 'json'
+		}
+
+	});
+
 }
+
+function procesarGeoInfo(datos){
+	console.log(datos);
+	var res = datos.query.results.Result;
+	var barrio = res.neighborhood;
+	var ciudad = res.city;
+	var pais = res.country;
+
+	$('#geo')
+		.prepend('<p><strong>'+barrio+'</strong><br>'+ciudad+', '+pais+'</p>');
+}
+
+
+
--- JavaScript/Geo.js
+++ JavaScript/Geo.js
@@ -3,14 +3,14 @@ $(function(){ //cuando ContentLoad este Listo !
 	var opciones = {}
 
 	function geo_error(){
-		alert("No puedo saber donde estas");
+		$('#geo').append('<p><strong>Error de Localizacion</strong><br /><a href="index.html">Recargar</a></p><hr />');
 	}
 
 	function geo_exito(posicion){
 		var lat = posicion.coords.latitude;
 		var longi = posicion.coords.longitude;
 		var mapa = new Image();
-		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=13&size=300x250&sensor=false&center="+lat+","+longi;
+		mapa.src = "http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=300x250&sensor=false&center="+lat+","+longi;
 		$('#geo').append(mapa);
 
 		obtenerGeoInfo(lat, longi);
